3bfb296f80a91771c66068c3ce3b8badc3263e54
jcari-dev <582119jc@gmail.com>
2024-03-24 20:27:02 +0000

    small moved a few function to a more appropiate location, giving a bit of organization to the Board.js file as it is getting too big

--- chess/src/components/main/board/Board.js
+++ chess/src/components/main/board/Board.js
@@ -7,6 +7,9 @@ import {
   validatePieceSelection,
   ValidateMove,
 } from "../../../utils/Validation";
+import { getValidMoves, getLeftAndRightSquares } from "../../../utils/Moves";
+import { initBoard } from "../../../utils/Board";
+import { adjacentEnemyPawn } from "../../../utils/Logic";
 
 function Board() {
   const [turnCount, setTurnCount] = useState(0);
@@ -15,153 +18,13 @@ function Board() {
   const [captureOccurred, setCaptureOccurred] = useState(false);
   const [halfmoveClock, setHalfmoveClock] = useState(0);
   const [castlingRights, setCastlingRights] = useState("KQkq");
-  const [enPassant, setEnPassant] = useState("");
-  const [boardData, setBoardData] = useState({
-    a8: { piece: "b_rook", color: "coral" },
-    b8: { piece: "b_knight", color: "white" },
-    c8: { piece: "b_bishop", color: "coral" },
-    d8: { piece: "b_queen", color: "white" },
-    e8: { piece: "b_king", color: "coral" },
-    f8: { piece: "b_bishop", color: "white" },
-    g8: { piece: "b_knight", color: "coral" },
-    h8: { piece: "b_rook", color: "white" },
-    a7: { piece: "b_pawn", color: "white" },
-    b7: { piece: "b_pawn", color: "coral" },
-    c7: { piece: "b_pawn", color: "white" },
-    d7: { piece: "b_pawn", color: "coral" },
-    e7: { piece: "b_pawn", color: "white" },
-    f7: { piece: "b_pawn", color: "coral" },
-    g7: { piece: "b_pawn", color: "white" },
-    h7: { piece: "b_pawn", color: "coral" },
-    a6: { piece: "", color: "coral" },
-    b6: { piece: "", color: "white" },
-    c6: { piece: "", color: "coral" },
-    d6: { piece: "", color: "white" },
-    e6: { piece: "", color: "coral" },
-    f6: { piece: "", color: "white" },
-    g6: { piece: "", color: "coral" },
-    h6: { piece: "", color: "white" },
-    a5: { piece: "", color: "white" },
-    b5: { piece: "", color: "coral" },
-    c5: { piece: "", color: "white" },
-    d5: { piece: "", color: "coral" },
-    e5: { piece: "", color: "white" },
-    f5: { piece: "", color: "coral" },
-    g5: { piece: "", color: "white" },
-    h5: { piece: "", color: "coral" },
-    a4: { piece: "", color: "coral" },
-    b4: { piece: "", color: "white" },
-    c4: { piece: "", color: "coral" },
-    d4: { piece: "", color: "white" },
-    e4: { piece: "", color: "coral" },
-    f4: { piece: "", color: "white" },
-    g4: { piece: "", color: "coral" },
-    h4: { piece: "", color: "white" },
-    a3: { piece: "", color: "white" },
-    b3: { piece: "", color: "coral" },
-    c3: { piece: "", color: "white" },
-    d3: { piece: "", color: "coral" },
-    e3: { piece: "", color: "white" },
-    f3: { piece: "", color: "coral" },
-    g3: { piece: "", color: "white" },
-    h3: { piece: "", color: "coral" },
-    a2: { piece: "w_pawn", color: "coral" },
-    b2: { piece: "w_pawn", color: "white" },
-    c2: { piece: "w_pawn", color: "coral" },
-    d2: { piece: "w_pawn", color: "white" },
-    e2: { piece: "w_pawn", color: "coral" },
-    f2: { piece: "w_pawn", color: "white" },
-    g2: { piece: "w_pawn", color: "coral" },
-    h2: { piece: "w_pawn", color: "white" },
-    a1: { piece: "w_rook", color: "white" },
-    b1: { piece: "w_knight", color: "coral" },
-    c1: { piece: "w_bishop", color: "white" },
-    d1: { piece: "w_queen", color: "coral" },
-    e1: { piece: "w_king", color: "white" },
-    f1: { piece: "w_bishop", color: "coral" },
-    g1: { piece: "w_knight", color: "white" },
-    h1: { piece: "w_rook", color: "coral" },
-  });
+  const [enPassant, setEnPassant] = useState("-");
+  const [validMoves, setValidMoves] = useState([]);
+  const [boardData, setBoardData] = useState(initBoard);
 
   function handleTurn() {
     setTurnOrder((prevState) => !prevState);
     setTurnCount(turnCount + 1);
-  }
-
-  function getLeftAndRightSquares(location) {
-    const file = location[0];
-
-    const fileToLeft = {
-      b: "a",
-      c: "b",
-      d: "c",
-      e: "d",
-      f: "e",
-      g: "f",
-      h: "g",
-      // 'a' has nothing to its left
-    };
-
-    const fileToRight = {
-      a: "b",
-      b: "c",
-      c: "d",
-      d: "e",
-      e: "f",
-      f: "g",
-      g: "h",
-      // 'h' has nothing to its right
-    };
-
-    const leftFile = fileToLeft[file];
-    const rightFile = fileToRight[file];
-    let squareToTheLeft = "";
-    let squareToTheRight = "";
-
-    if (leftFile) {
-      squareToTheLeft = leftFile + location[1];
-    }
-
-    if (rightFile) {
-      squareToTheRight = rightFile + location[1];
-    }
-
-    return {
-      squareToTheLeft: squareToTheLeft,
-      squareToTheRight: squareToTheRight,
-    };
-  }
-
-  function adjacentEnemyPawn(
-    board,
-    pawnToCheck,
-    squareToTheLeft,
-    squareToTheRight
-  ) {
-    console.log("Checking for adjacent pawns at: ", pawnToCheck);
-    const colorOfPawnToCheck = pawnToCheck[0]; // should be either b or w
-    const result = {};
-    result[squareToTheLeft] = false;
-    result[squareToTheRight] = false;
-    if (
-      squareToTheLeft &&
-      board[squareToTheLeft].piece[0] !== colorOfPawnToCheck
-    ) {
-      result[squareToTheLeft] = true;
-    }
-
-    if (
-      squareToTheRight &&
-      board[squareToTheRight].piece[0] !== colorOfPawnToCheck
-    ) {
-      result[squareToTheRight] = true;
-    }
-
-    if (result[squareToTheLeft] && result[squareToTheRight]) {
-      return result;
-    }
-
-    return false;
   }
 
   function handleEnPassant(board, movingFrom, movedTo) {
@@ -208,7 +71,7 @@ function Board() {
         return movedTo[0] + conditions[startRank];
       }
     }
-    setEnPassant("");
+    setEnPassant("-");
     return false;
   }
 
@@ -270,6 +133,17 @@ function Board() {
         if (firstClick === null) {
           setFirstClick(notation);
           console.log(piece, "was selected");
+          const validMoves = await getValidMoves({
+            board: boardData,
+            pieceMoving: boardData[notation].piece,
+            target: notation,
+            turnCount: turnCount,
+            halfmoveClock: halfmoveClock,
+            castlingRights: castlingRights,
+            enPassant: enPassant,
+          });
+
+          setValidMoves(validMoves);
         } else {
           // Move the piece to the new square
           const pieceToMove = boardData[firstClick].piece;
@@ -283,8 +157,8 @@ function Board() {
             castlingRights: castlingRights,
             enPassant: enPassant,
           });
-
-          if (isMoveValid) {
+          console.log(validMoves, notation, "MOVING VALIDATION");
+          if (validMoves.includes(notation)) {
             console.log(pieceToMove, "moved into", notation);
 
             setBoardData((prev) => ({
@@ -324,6 +198,8 @@ function Board() {
             setFirstClick(null); // Reset the first click
 
             console.log(boardData);
+          } else {
+            console.log("Illegal move.");
           }
         }
       } else {
