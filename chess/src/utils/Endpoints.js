b1a3b1bc5d30d282b57812090f4763de682d934c
Jorge Caridad <65676916+jcari-dev@users.noreply.github.com>
2024-04-03 21:03:38 -0400

    Revert "Update/cpu"

--- chess/src/utils/Endpoints.js
+++ chess/src/utils/Endpoints.js
@@ -15,8 +15,6 @@ const endpoints = {
   validateMove: `${API_BASE_URL}${process.env.REACT_APP_VALIDATE_MOVE_ENDPOINT}`,
   vitals: `${API_BASE_URL}${process.env.REACT_APP_VITALS}`,
   getProfile: `${API_BASE_URL}${process.env.REACT_APP_GET_PROFILE_ENDPOINT}`,
-  cpuCreateRoom: `${API_BASE_URL}${process.env.REACT_APP_CPU_CREATE_ROOM}`,
-  checkTurnCpu: `${API_BASE_URL}${process.env.REACT_APP_CHECK_TURN_CPU_ENDPOINT}`
 };
 
 export default endpoints;
